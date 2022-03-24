b906a1a73cd0cec72ded23aaddeed25b5efa389c
Giselle Salvador <gisellesalvadorx@gmail.com>
2022-03-23 20:10:15 -0400

    Initial Commit

---
+++ MoviesListUITests/MoviesListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesListUITestsLaunchTests.swift
+//  MoviesListUITests
+//
+//  Created by Giselle Salvador on 2022-03-23.
+//
+
+import XCTest
+
+class MoviesListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
